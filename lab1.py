8efd2cfb897537a47bb7f5c48e50b94a81b7d4b7
Meilin-Lyu <mlyu@ualberta.ca>
2021-01-13 23:28:11 -0700

    question11

--- lab1.py
+++ lab1.py
@@ -5,3 +5,10 @@ print(requests.__version__)
 url = "http://google.com/"
 r= requests.get(url)
 print(r)
+
+url1 = 'https://raw.githubusercontent.com/Meilin-Lyu/cmput404_lab1/main/lab1.py'
+urllib.request.urlretrieve(url1,"lab1_m.py")
+f=open("lab1_m.py") 
+content = f.read()                 
+print(content)                       
+f.close()
